400f82f7e2ccbef78809006ee5b67b6794642e80
danilopetrovic88 <daniloljubica14@gmail.com>
2024-02-14 19:29:47 +0100

    changes

--- controllers/cars/deleteCarController.js
+++ controllers/cars/deleteCarController.js
@@ -4,6 +4,7 @@ const db = mongojs("carsapp", ["users", "cars"]);
 const deleteCarController = (req,res) => {
     let carId = req.params.carId;
 
+
     db.cars.remove({ _id: mongojs.ObjectID(carId) }, (err, docs) => {
         res.send("Deleted")
     });
